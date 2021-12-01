9eb50cc9b4fe88328418c1d7b2e7255052f0ae04
Julius van Dis <vandis.j@gmail.com>
2021-12-01 09:12:44 +0100

    Use windowed instead of ugly folds :)

--- src/main/kotlin/nl/zzave/adventofcode/twentytwentyone/Day1.kt
+++ src/main/kotlin/nl/zzave/adventofcode/twentytwentyone/Day1.kt
@@ -9,39 +9,32 @@ object Day1 {
     fun solvePart1(input: List<String>): Int {
         val depthMeasurements: List<Int> = input.map { parseInt(it) }
 
+        return countDeeperEntries(depthMeasurements)
+
+    }
+
+    /**
+     * An entry (list item) is deeper if it is larger then the previous entry
+     */
+    private fun countDeeperEntries(depthMeasurements: List<Int>): Int {
         return depthMeasurements.zipWithNext { a, b ->
             println("a $a vs b $b (= ${a < b})")
             if (a < b) 1 else 0
         }.sum()
-
     }
 
     fun solvePart2(input: List<String>): Int {
         val depthMeasurements: List<Int> = input.map { parseInt(it) }
         println(depthMeasurements)
 
-        // TODO: y u so ugly?
-        val depths = depthMeasurements.fold(Depths(mutableListOf(), mutableListOf())) { all, cur ->
-            all.ongoing.add(cur)
-            if (all.ongoing.size == 3) {
-                all.depths.add(all.ongoing.sum())
-                all.ongoing.removeAt(0)
-            }
-            all
-        }
-
+        val depths = depthMeasurements.windowed(size = 3).map { it.sum() }
 
         println()
-        println(depths.depths)
+        println(depths)
 
-        return solvePart1(depths.depths.map {"$it" })
+        return countDeeperEntries(depths)
 
     }
-
-    data class Depths(
-        val depths: MutableList<Int>,
-        val ongoing: MutableList<Int>
-    )
 }
 
 fun main() {
