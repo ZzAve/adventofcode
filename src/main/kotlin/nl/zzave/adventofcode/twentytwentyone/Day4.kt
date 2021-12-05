329ad718d3a391b9548149f57b086de467390bf0
Julius van Dis <vandis.j@gmail.com>
2021-12-05 08:31:34 +0100

    Day 4 runningFold stuff

--- src/main/kotlin/nl/zzave/adventofcode/twentytwentyone/Day4.kt
+++ src/main/kotlin/nl/zzave/adventofcode/twentytwentyone/Day4.kt
@@ -6,7 +6,6 @@ import nl.zzave.adventofcode.transposeMatrix
 object Day4 : TwentyTwentyOneProblem<Int> {
     override var debugMode: Boolean = false
 
-
     override fun solvePart1(input: List<String>): Int {
         val (numbersToCall, bingoBoards) = input.toDomainModel()
         bingoBoards.forEach { it.prettyPrint() }
@@ -15,84 +14,42 @@ object Day4 : TwentyTwentyOneProblem<Int> {
         return winningBoard?.calculateScore(calledNumbers) ?: -1
     }
 
+    private fun callNumbersUntilBingo(
+        numbersToCall: List<Int>,
+        bingoBoards: List<BingoBoard>
+    ): Pair<BingoBoard?, List<Int>> = numbersToCall
+        .runningFold(emptyList<Int>()) { calledNumbers, newNumber -> calledNumbers + listOf(newNumber) }
+        .asSequence()
+        .drop(1)
+        .map { calledNumbersSoFar ->
+            debug("Processing ${calledNumbersSoFar.last()} ($calledNumbersSoFar)")
+            bingoBoards.firstOrNull { it.hasBingo(calledNumbersSoFar) } to calledNumbersSoFar
+        }
+        .firstOrNull { it.first != null }
+        ?: (null to numbersToCall)
+
     override fun solvePart2(input: List<String>): Int {
         val (numbersToCall, bingoBoards) = input.toDomainModel()
 //        bingoBoards.forEach { it.prettyPrint() }
 
         val (losingBoard, calledNumbers) = callNumbersReverseUntilNoBingo(numbersToCall, bingoBoards)
         return losingBoard?.calculateScore(calledNumbers) ?: -1
-
     }
 
     private fun callNumbersReverseUntilNoBingo(
         numbersToCall: List<Int>,
         bingoBoards: List<BingoBoard>
     ): Pair<BingoBoard?, List<Int>> {
-        val allNumbers = numbersToCall.asReversed().fold(numbersToCall.toMutableList()) { calledNumbers, lastNumber ->
-            calledNumbers -= lastNumber
-            bingoBoards.forEach {
-                if (!it.hasBingo(calledNumbers)) {
-                    calledNumbers += lastNumber // Add it again, as this is the magic Bingo number
-                    return it to calledNumbers
-                }
+        return numbersToCall.asReversed()
+            .runningFold(numbersToCall) { calledNumbers, newNumber -> calledNumbers - listOf(newNumber) }
+            .asSequence()
+            .map { calledNumbersSoFar ->
+                debug("Processing ${calledNumbersSoFar[calledNumbersSoFar.size - 2]} ($calledNumbersSoFar)")
+                bingoBoards.firstOrNull { !it.hasBingo(calledNumbersSoFar - listOf(calledNumbersSoFar.last())) } to calledNumbersSoFar
             }
-            calledNumbers
-        }
-
-        return null to allNumbers
+            .firstOrNull { it.first != null }
+            ?: (null to numbersToCall)
     }
-
-
-    private fun callNumbersUntilBingo(
-        numbersToCall: List<Int>,
-        bingoBoards: List<BingoBoard>
-    ): Pair<BingoBoard?, List<Int>> {
-        val allNumbers = numbersToCall.fold(mutableListOf<Int>()) { calledNumbers, newNumber ->
-            calledNumbers += newNumber
-            bingoBoards.forEach {
-                if (it.hasBingo(calledNumbers)) {
-                    return it to calledNumbers
-                }
-            }
-            calledNumbers
-        }
-
-        return null to allNumbers
-    }
-
-
-    data class BingoBoard(
-        val board: List<List<Int>>,
-        val transposedBoard: List<List<Int>>
-    ) {
-
-        fun prettyPrint() {
-            log("--- BingoBoard ---")
-            printMatrix(this.board)
-            log("------------------")
-        }
-
-        fun hasBingo(calledNumbers: List<Int>): Boolean {
-            // check rows (stop if number is missed, return if full row found)
-            val aRowHasBingo: Boolean = board.fold(false) { acc, row ->
-                acc || calledNumbers.containsAll(row).also { debug("Row $row makes bingo?\t$it ") }
-            }
-
-            // check columns (stop if number is missed, return if full row found)
-            return aRowHasBingo || transposedBoard.fold(false) { acc, col ->
-                acc || calledNumbers.containsAll(col).also { debug("Column $col makes bingo?\t$it") }
-            }
-        }
-
-        fun calculateScore(calledNumbers: List<Int>): Int {
-            val unmarkedNumbers =
-                board.fold(0) { total, row -> total + row.filter { !calledNumbers.contains(it) }.sum() }
-            log("Sum of unmarked numbers: $unmarkedNumbers")
-            log("Last number called: ${calledNumbers.last()}")
-            return unmarkedNumbers * calledNumbers.last()
-        }
-    }
-
 
     /**
      * @see twentytwentyone.day4.data for input data
@@ -123,6 +80,37 @@ object Day4 : TwentyTwentyOneProblem<Int> {
 
 
         return calledNumbers to bingoBoards
+    }
+
+
+    data class BingoBoard(
+        val board: List<List<Int>>,
+        val transposedBoard: List<List<Int>>
+    ) {
+
+        fun hasBingo(calledNumbers: List<Int>): Boolean {
+            val aRowHasBingo: Boolean = board.fold(false) { acc, row ->
+                acc || calledNumbers.containsAll(row).also { debug("Row $row makes bingo?\t$it ") }
+            }
+
+            return aRowHasBingo || transposedBoard.fold(false) { acc, col ->
+                acc || calledNumbers.containsAll(col).also { debug("Column $col makes bingo?\t$it") }
+            }
+        }
+
+        fun calculateScore(calledNumbers: List<Int>): Int {
+            val unmarkedNumbers =
+                board.fold(0) { total, row -> total + row.filter { !calledNumbers.contains(it) }.sum() }
+            log("Sum of unmarked numbers: $unmarkedNumbers")
+            log("Last number called: ${calledNumbers.last()}")
+            return unmarkedNumbers * calledNumbers.last()
+        }
+
+        fun prettyPrint() {
+            log("--- BingoBoard ---")
+            printMatrix(this.board)
+            log("------------------")
+        }
     }
 }
 
