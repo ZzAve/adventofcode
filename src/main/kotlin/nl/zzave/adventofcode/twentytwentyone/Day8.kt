a36892e3798ac9e113f4460a29169bae94ed6a1f
Julius van Dis <vandis.j@gmail.com>
2021-12-08 19:37:59 +0100

    Day 8 refactor to get rid of bangs, but it's still ugly...

--- src/main/kotlin/nl/zzave/adventofcode/twentytwentyone/Day8.kt
+++ src/main/kotlin/nl/zzave/adventofcode/twentytwentyone/Day8.kt
@@ -24,177 +24,101 @@ object Day8 : TwentyTwentyOneProblem<Long> {
     }
 
     private fun findMapping(display: Display): Int {
+
+        debug("----------------")
+        debug("Finding mapping for $display")
         val numberMapping = mutableMapOf<Int, Set<Char>>()
+        val one = display.signalPattern.first { it.size == 2 }.also { numberMapping[1] = it }
+        val four = display.signalPattern.first { it.size == 4 }.also { numberMapping[4] = it }
+        val seven = display.signalPattern.first { it.size == 3 }.also { numberMapping[7] = it }
+        val eight = display.signalPattern.first { it.size == 7 }.also { numberMapping[8] = it }
 
-        numberMapping[1] = display.signalPattern.first { it.size == 2 }
-        numberMapping[4] = display.signalPattern.first { it.size == 4 }
-        numberMapping[7] = display.signalPattern.first { it.size == 3 }
-        numberMapping[8] = display.signalPattern.first { it.size == 7 }
-
-        //  dddd      0000
-        // e    a    1    2
-        // e    a    1    2
-        //  ffff      3333
-        // g    b    4    5
-        // g    b    4    5
-        //  cccc      6666
+        //  dddd
+        // e    a
+        // e    a
+        //  ffff
+        // g    b
+        // g    b
+        //  cccc
         val positionMapping = mutableMapOf<Position, Char>()
 
-
         // From 1 and 7, you can derive the top
-        val top: Char = (numberMapping[7]!! - numberMapping[1]!!).first()
+        val top: Char = (seven - one).first()
         positionMapping[Position.TOP] = top
-
+        debug("$numberMapping")
+        debug("$positionMapping")
 
         // From 4 and 1 you can derive center and top left
-        val centerAndTopLeft: Set<Char> = (numberMapping[4]!! - numberMapping[1]!!)
+        val centerAndTopLeft: Set<Char> = (four - one)
 
-
-        //------
-        // - top
-        // - top right + bottom right (1)
-        // - center + top left
-
-
-        // - you can derive 9 (contains all 3 elements from above + 1 additional ones)
-        // - you can derive 9 (contains a 4, top, + 1 additional element, and it's not 8)
-        // - you can derive bottom
+        // Six, nine, and zero al have 6 six signals
         val six = display.signalPattern
             .first {
                 !numberMapping.values.contains(it) &&
                         it.size == 6 &&
-                        !it.containsAll(numberMapping[7]!!)
+                        !it.containsAll(seven)
 
-            }
-        numberMapping[6] = six
+            }.also { numberMapping[6] = it }
 
         val nine = display.signalPattern
             .first {
                 !numberMapping.values.contains(it) &&
                         it.size == 6 &&
-                        it.containsAll(numberMapping[7]!!) &&
+                        it.containsAll(seven) &&
                         it.containsAll(centerAndTopLeft)
 
-            }
-        numberMapping[9] = nine
+            }.also { numberMapping[9] = it }
 
         val zero = display.signalPattern
             .first {
                 !numberMapping.values.contains(it) &&
                         it.size == 6
-            }
-        numberMapping[0] = zero
+            }.also { numberMapping[0] = it }
 
 
-        val bottomLeft = numberMapping[8]!! - numberMapping[9]!!
+        val bottomLeft = eight - nine
         check(bottomLeft.size == 1)
         positionMapping[Position.BOTTOM_LEFT] = bottomLeft.first()
-        debug("$numberMapping")
-        debug("$positionMapping")
 
-        val center = numberMapping[8]!! - numberMapping[0]!!
+        val center = eight - zero
         check(center.size == 1)
         positionMapping[Position.CENTER] = center.first()
-        debug("$numberMapping")
-        debug("$positionMapping")
 
-        val topRight = numberMapping[8]!! - numberMapping[6]!!
+        val topRight = eight - six
         check(topRight.size == 1)
         positionMapping[Position.TOP_RIGHT] = topRight.first()
         debug("$numberMapping")
         debug("$positionMapping")
 
 
-        // - you can derive 3 (contains 4, bottom and top, minus 1 element from four)
-        val five = display.signalPattern.first {
+        // five, two, and three all have 5 signals
+        display.signalPattern.first {
             !numberMapping.values.contains(it) &&
                     it.size == 5 &&
                     !it.contains(positionMapping[Position.BOTTOM_LEFT]) &&
                     !it.contains(positionMapping[Position.TOP_RIGHT])
-        }
-        numberMapping[5] = five
+        }.also { numberMapping[5] = it }
 
-        // - you can derive 3 (contains 4, bottom and top, minus 1 element from four)
-        val two = display.signalPattern.first {
+
+        display.signalPattern.first {
             !numberMapping.values.contains(it) &&
                     it.size == 5 &&
                     it.contains(positionMapping[Position.BOTTOM_LEFT])
-
-        }
-        numberMapping[2] = two
+        }.also { numberMapping[2] = it }
 
         debug("Mapped everything but 3?")
+        check(numberMapping.size == 9)
+        check(numberMapping[3] == null)
         debug("$numberMapping")
         debug("$positionMapping")
 
-        val three = display.signalPattern.first {
+        display.signalPattern.first {
             !numberMapping.values.contains(it) &&
                     it.size == 5 &&
                     !it.contains(positionMapping[Position.BOTTOM_LEFT]) &&
                     it.contains(positionMapping[Position.TOP_RIGHT])
 
-        }
-        numberMapping[3] = three
-
-
-
-//
-//        // - you can derive 3 (contains 4, bottom and top, minus 1 element from four)
-//        val three = display.signalPattern.first {
-//            !numberMapping.values.contains(it) &&
-//                    it.size == 5 &&
-//                    numberMapping.values.none { n -> n == it } &&
-//                    (numberMapping[4]!! subtract it).size == 1 && // contains all but 1 elements from 4
-//                    it.contains(positionMapping[Position.TOP]) &&
-//                    it.contains(positionMapping[Position.BOTTOM]) &&
-//                    it.size == 5
-//
-//        }
-//        numberMapping[3] = three
-//
-//        // - you can derive top left
-//        val topLeft = numberMapping[4]!! - numberMapping[3]!!
-//        check(topLeft.size == 1)
-//        positionMapping[Position.TOP_LEFT] = topLeft.first()
-//
-//
-//        debug("$numberMapping")
-//        debug("$positionMapping")
-//
-//        // ---------
-//        // - top
-//        // - top right + bottom right
-//        // - bottom
-//        // - center
-//        // - top left
-//
-//
-//        // you can derive 2
-//        val two = display.signalPattern.first {
-//            !numberMapping.values.contains(it) &&
-//                    it.size == 5 &&
-//                    it.contains(positionMapping[Position.BOTTOM_LEFT]) &&
-//                    it.contains(positionMapping[Position.TOP]) &&
-//                    it.contains(positionMapping[Position.CENTER]) &&
-//                    it.contains(positionMapping[Position.BOTTOM])
-//
-//        }
-//        numberMapping[2] = two
-//
-//        // you can derive bottom right
-//        val bottomRight = numberMapping[1]!! - numberMapping[2]!!
-//        check(bottomRight.size == 1)
-//        positionMapping[Position.TOP_RIGHT] = bottomRight.first()
-//
-//
-//        // you can derive everything
-//        check(numberMapping.size == 8)
-//        val five = display.signalPattern.first {
-//            !numberMapping.values.contains(it) && it.size == 5
-//        }
-//        numberMapping[5] = five
-//
-
+        }.also { numberMapping[3] = it }
 
         debug("Unique mapping: $numberMapping")
         // Final mapping
@@ -204,7 +128,7 @@ object Day8 : TwentyTwentyOneProblem<Long> {
                 .also { debug("${it.value}: ${it.key}") }
                 .key
         }.reduce { acc, i -> acc * 10 + i }
-            .also { debug("${display.code} : $it") }
+            .also { d -> log("${display.code.map{ it.joinToString("")}} : $d") }
     }
 
     private fun parseDisplayInput(input: List<String>): List<Display> = input
@@ -228,9 +152,9 @@ object Day8 : TwentyTwentyOneProblem<Long> {
     }
 
 
+    private fun String.sorted(): Set<Char> = toCharArray().toSet()
 }
 
-private fun String.sorted(): Set<Char> = toCharArray().toSet()
 
 fun main() {
     Day8.debugMode = true
